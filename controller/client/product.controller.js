7a00b04d472b9186f2beef6634d68e5957d358d8
AdamFakee <buidinhtuan04@gmail.com>
2024-06-08 09:24:28 +0700

    đổ data ra giao diện

--- controller/client/product.controller.js
+++ controller/client/product.controller.js
@@ -1,9 +1,12 @@
 const products = require('../../models/product.model');
 module.exports.index = async (req, res) => {
   const productList = await products.find({});
-  console.log(productList)
+  for (const item of productList) {
+    item.newPrice = ((1 - item.discountPercentage/100) * item.price).toFixed(0);
+  }
   res.render('client/pages/products/index', {
     pageTitle: 'trang danh sach san pham',
     productList:productList
   });
 }
+
