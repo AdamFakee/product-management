edd272fa4f5cb8c63290d891d5774f43971d38b7
AdamFakee <buidinhtuan04@gmail.com>
2024-06-10 23:28:20 +0700

    logic tính năng lọc sản phẩm theo trạng thái - FE

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,1 +1,26 @@
-console.log("OK");
+// button-status
+const buttonStatus = document.querySelectorAll('[button-status]');
+if(buttonStatus.length > 0) {
+    let url = new URL(window.location.href);  // copy link 
+    buttonStatus.forEach(button => {
+        button.addEventListener('click', () => {
+            const status = button.getAttribute("button-status"); // lay noi dung class
+            if(status){
+                url.searchParams.set("status", status);  // them vao link cap name=value
+            } else {
+                url.searchParams.delete('status'); // xoa 
+            }
+            window.location.href = url.href;
+        })
+    })
+
+    // end button status
+
+    // them active mac dinh
+    const statusCurrent = url.searchParams.get("status") || "";
+    const buttonCurrent = document.querySelector(`[button-status="${statusCurrent}"]`);
+    if(buttonCurrent){
+        buttonCurrent.classList.add('active');
+    }
+}
+
