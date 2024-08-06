d75a5ff920c4de3acf5bc52bebf115f7ade8058e
AdamFakee <buidinhtuan04@gmail.com>
2024-08-06 13:32:56 +0700

    load tin nhắn cũ - k load hết từ đầu (web load chậm)

--- controller/client/chat.controller.js
+++ controller/client/chat.controller.js
@@ -1,9 +1,13 @@
 const Chat = require("../../models/chat.model");
 const User = require("../../models/user.model");
+const paginationHelper = require('../../helpers/pagination.helper');
 
 // [GET] /chat
 module.exports.index = async (req, res) => {
     const userId = res.locals.user.id;
+    const limitMess = 20; // chỉ hiển thị 20 tin nhắn
+    const paginationMessage = await paginationHelper(req, {}, Chat, limitMess); // phân trang của mess
+    const totalPageMess = paginationMessage.totalPage;  // tổng số trang có thể load 
     // SocketIO
     _io.once("connection", (socket) => {  // người dùng kết nối đến 1 lần
         // CLIENT_SEND_MESSAGE
@@ -24,21 +28,38 @@ module.exports.index = async (req, res) => {
             // Trả tin nhắn realtime về cho mọi người 
             _io.emit('SERVET_SEND_MESSAGE', chatData);
         })
-    
+        //CLIENT_LOAD_MORE_MESSAGE
+        socket.on('CLIENT_LOAD_MORE_MESSAGE', async (currentPage) => {          // người dùng yêu cầu xem thêm tin nhắn lúc trước
+            const skipMess = (parseInt(totalPageMess - currentPage))*limitMess;
+            const chats = await Chat.find({}).sort({
+                createdAt : 'desc'
+            }).skip(skipMess).limit(limitMess);  // lấy ra số lượng tin nhắn - tạo sau lấy trước
+            for (const chat of chats) {
+                const infoUser = await User.findOne({
+                    _id: chat.userId
+                });
+                chat.fullName = infoUser.fullName;
+            }
+            socket.emit('SERVER_SEND_MORE_MESS', chats); // server trả tin nhắn về cho người dùng
+        })
+        // End CLIENT_LOAD_MORE_MESSAGE
       });
         // End SocketIO
         
-        const chats = await Chat.find({}); 
-        
-        for (const chat of chats) {
-            const infoUser = await User.findOne({
-                _id: chat.userId
-            });
-        
-            chat.fullName = infoUser.fullName;
-        }
+    const chats = await Chat.find({}).sort({
+        createdAt : 'desc'
+    }).limit(limitMess);  // hiển thị số tin nhắn khi người dùng vô lần đầu
     
+    for (const chat of chats) {
+        const infoUser = await User.findOne({
+            _id: chat.userId
+        });
+    
+        chat.fullName = infoUser.fullName; // thêm tên - nếu là người khác gửi đến thì hiển thị thêm tên
+        }
+    chats.reverse(); // in ra xem vì sao cần đảo ngược
     res.render('client/pages/chat/index.pug', {
-        chats : chats
+        chats : chats, 
+        paginationMessage : paginationMessage
     });
 }
--- public/client/js/chat.js
+++ public/client/js/chat.js
@@ -43,3 +43,40 @@ if(chatBody) {
   chatBody.scrollTop = chatBody.scrollHeight;
 }
 // End Scroll Chat To Bottom
+
+// load thêm tin nhắn cũ
+if(chatBody){
+  chatBody.addEventListener('scroll', ()=> { // lắng nghe scroll
+    const scrollValue = chatBody.scrollTop;
+    if(scrollValue == 0){  // scroll lên trên hết mức có thể
+      const currentPage = document.querySelector('[current-page]').getAttribute('current-page'); // trang hiển thị message hiện tại
+      let totalPage = document.querySelector('[total-page]').getAttribute('total-page'); // lấy tổng số trang có thể load đc
+      totalPage = parseInt(totalPage);
+      if(currentPage < totalPage){
+        socket.emit('CLIENT_LOAD_MORE_MESSAGE', currentPage); // phát ra yêu cầu cho sever
+        socket.on('SERVER_SEND_MORE_MESS', (data) => {
+          const userId = document.querySelector('[my-id]').getAttribute('my-id'); // id người dùng
+          data.forEach(mess => {
+            const oldLine = document.createElement('div'); // dòng tin nhắn cũ cần load
+            let fullName = '';
+            if(mess.userId == userId){ // mình nhắn
+              oldLine.classList.add('inner-outgoing');
+            } else { // người khác nhắn đến
+              oldLine.classList.add('inner-incoming'); 
+              fullName = `<div class="inner-name"> ${mess.fullName} </div> ` // hiển thị fullname
+            }
+            oldLine.innerHTML = `
+              ${fullName}
+              <div class='inner-content'> ${mess.content} </div>
+            `
+            chatBody.insertBefore(oldLine, chatBody.firstElementChild) // chèn lên đầu
+          });
+        })
+        if(currentPage <= totalPage - 1){
+          document.querySelector('[current-page]').setAttribute('current-page', parseInt(currentPage)+1); // cập nhập currentPage
+        }
+      }
+    }
+  })
+}
+// end load thêm tin nhắn cũ
